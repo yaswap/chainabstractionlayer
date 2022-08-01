cc279df99df8ddb01a73cf805a58a194ba0a1e83
Chau Nguyen <chaunguyen4392@gmail.com>
2022-08-01 21:10:57 +0700

    Revert "Debug hangup"
    This reverts commit 363412e59b9993c8a515edef9d0daa6eec46fe3c.

--- packages/yacoin-esplora-api-provider/lib/YacoinEsploraApiProvider.ts
+++ packages/yacoin-esplora-api-provider/lib/YacoinEsploraApiProvider.ts
@@ -64,9 +64,7 @@ export default class YacoinEsploraApiProvider extends NodeProvider implements Pa
   }
 
   async _getUnspentTransactions(address: string): Promise<yacoin.UTXO[]> {
-    console.log("TACA ===> YacoinEsploraApiProvider.ts, _getUnspentTransactions, getting for address ", address)
     const data: esplora.UTXO[] = await this.nodeGet(`/address/${address}/utxo`)
-    console.log("TACA ===> YacoinEsploraApiProvider.ts, _getUnspentTransactions, complete getting for address ", address)
     return data.map((utxo) => ({
       ...utxo,
       address,
@@ -91,9 +89,7 @@ export default class YacoinEsploraApiProvider extends NodeProvider implements Pa
   }
 
   async _getAddressTransactionCount(address: string) {
-    console.log("TACA ===> YacoinEsploraApiProvider.ts, _getAddressTransactionCount, getting for address ", address)
     const data: esplora.Address = await this.nodeGet(`/address/${address}`)
-    console.log("TACA ===> YacoinEsploraApiProvider.ts, _getAddressTransactionCount, address ", address, " has ", data.tx_count, " txcount")
     return data.tx_count
   }
 
