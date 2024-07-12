c6fc3f8338e5bc6fef42e3893a5a355768bb9b82
Nguyen Thai Chau <chaunguyen4392@gmail.com>
2024-07-12 11:37:52 +0700

    Support send expired timelock UTXO in YacoinSingleWallet

--- packages/yacoin/lib/wallet/YacoinSingleWallet.ts
+++ packages/yacoin/lib/wallet/YacoinSingleWallet.ts
@@ -813,7 +813,11 @@ export class YacoinSingleWallet extends Wallet<any, any> implements IYacoinWalle
     var tx = new TransactionBuilder(network);
     // Add input
     for (let i = 0; i < inputs.length; i++) {
-      tx.addInput(inputs[i].txid, inputs[i].vout);
+      const timelockinfo = inputs[i].timelockinfo
+      if (timelockinfo && timelockinfo.locktime > 0) {
+        tx.setLockTime(timelockinfo.locktime)
+      }
+      tx.addInput(inputs[i].txid, inputs[i].vout, timelockinfo?.sequence)
     }
 
     // Add output
